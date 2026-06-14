0db052a8fcaad77dad1c8492655d745add69ac8c
Game-Dev-Shubham <shubhamyadav1247878@gmail.com>
2026-06-14 21:41:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-const API_KEY = process.env.GEMINI_API_KEY;
+const API_KEY = AIzaSyDoOAHtv3b9ZpTpqY3LtNPbbdX_A5kEY1g;
 
 app.post("/chat", async (req, res) => {
 
