28f2b2a4c7cbc9ef7119ef6707db185f64dbb13e
Game-Dev-Shubham <shubhamyadav1247878@gmail.com>
2026-06-14 22:10:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 
 const app = express();
-
 app.use(express.json());
 
-// Testing only
+app.get("/", (req, res) => {
+    res.send("Server Working");
+});
+
+app.get("/chat", (req, res) => {
+    res.send("Use POST request");
+});
+
 const API_KEY = "AIzaSyDoOAHtv3b9ZpTpqY3LtNPbbdX_A5kEY1g";
 
 app.post("/chat", async (req, res) => {
 
     try {
-
-        const message = req.body.message;
 
         const response = await fetch(
             "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent",
@@ -22,15 +26,11 @@ app.post("/chat", async (req, res) => {
                     "X-goog-api-key": API_KEY
                 },
                 body: JSON.stringify({
-                    contents: [
-                        {
-                            parts: [
-                                {
-                                    text: message
-                                }
-                            ]
-                        }
-                    ]
+                    contents: [{
+                        parts: [{
+                            text: req.body.message
+                        }]
+                    }]
                 })
             }
         );
@@ -43,8 +43,7 @@ app.post("/chat", async (req, res) => {
             data.candidates &&
             data.candidates[0] &&
             data.candidates[0].content &&
-            data.candidates[0].content.parts &&
-            data.candidates[0].content.parts[0]
+            data.candidates[0].content.parts
         ) {
             reply =
                 data.candidates[0]
@@ -56,25 +55,14 @@ app.post("/chat", async (req, res) => {
             reply: reply
         });
 
-    } catch (err) {
-
-        console.error(err);
+    } catch (e) {
 
         res.status(500).json({
-            error: err.message
+            error: e.toString()
         });
 
     }
 
 });
 
-const PORT =
-    process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-
-    console.log(
-        "Server running on port " + PORT
-    );
-
-});
+app.listen(process.env.PORT || 3000);
