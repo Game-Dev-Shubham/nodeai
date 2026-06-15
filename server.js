b53e6e32d61a8e19dcd6e48cf4e3a8baf0a6a06c
Game-Dev-Shubham <shubhamyadav1247878@gmail.com>
2026-06-15 11:14:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,50 @@
 const http = require("http");
-const url = require("url");
+const { URL } = require("url");
 
-var users = [];
-var messages = [];
-
-function sendJson(res, obj) {
-    res.writeHead(200, {
-        "Content-Type": "application/json"
-    });
-    res.end(JSON.stringify(obj));
-}
+const users = [];
+const messages = [];
 
 const server = http.createServer(function(req, res) {
 
-    var parsed = url.parse(req.url, true);
-    var path = parsed.pathname;
+    const url = new URL(req.url, "http://localhost:3000");
+    const path = url.pathname;
+
+    console.log("Request:", path);
 
     if (path === "/join") {
 
-        var name = parsed.query.name;
+        const name = url.searchParams.get("name");
 
         if (name) {
 
             if (users.indexOf(name) === -1) {
-
                 users.push(name);
-
-                messages.push({
-                    sender: "SERVER",
-                    text: name + " joined chat"
-                });
             }
+
+            messages.push({
+                sender: "SERVER",
+                text: name + " joined chat"
+            });
+
+            console.log("User Joined:", name);
+            console.log(messages);
+
+            res.writeHead(200, {
+                "Content-Type": "text/plain"
+            });
+
+            res.end("JOINED");
+            return;
         }
 
-        res.end("OK");
+        res.end("Name missing");
         return;
     }
 
     if (path === "/send") {
 
-        var name = parsed.query.name;
-        var text = parsed.query.text;
+        const name = url.searchParams.get("name");
+        const text = url.searchParams.get("text");
 
         if (name && text) {
 
@@ -48,21 +52,45 @@ const server = http.createServer(function(req, res) {
                 sender: name,
                 text: text
             });
+
+            console.log("Message:", name, ":", text);
+            console.log(messages);
+
+            res.writeHead(200, {
+                "Content-Type": "text/plain"
+            });
+
+            res.end("SENT");
+            return;
         }
 
-        res.end("SENT");
+        res.end("Missing parameters");
         return;
     }
 
     if (path === "/messages") {
 
-        sendJson(res, messages);
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        });
+
+        res.end(JSON.stringify(messages));
         return;
     }
 
-    res.end("Chat Server Running");
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+
+    res.end(
+        "Chat Server Running\n\n" +
+        "/join?name=YourName\n" +
+        "/send?name=YourName&text=Hello\n" +
+        "/messages"
+    );
+
 });
 
 server.listen(3000, function() {
-    console.log("Server running on port 3000");
+    console.log("Server Started On Port 3000");
 });
