b2fb55c12d61b7662e39fe174a515ae8944a68c0
Game-Dev-Shubham <shubhamyadav1247878@gmail.com>
2026-06-16 18:07:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = 3000;
 var messages = [];
 
 http.createServer(function(req, res) {
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET");
+res.setHeader("Access-Control-Allow-Headers", "*");
 
     var parsed = url.parse(req.url, true);
 
